2a4ab12db65870ea628cc0709e8c7c236a64de12
Jonathan Byrne <jon.byrne27@btinternet.com>
2024-01-27 22:42:36 +0000

    Comments added

--- MandelbrotSet/ConsoleApplication1/ConsoleApplication1.cpp
+++ MandelbrotSet/ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,7 +15,7 @@ double lerp(int bounda, int boundb, double axis) {
 }
 bool modulus(double r, double i) {
     double mod = sqrt((r * r) + (i * i)); //determining the modulus to check if it is within the set
-    if (mod > 10) return true; //zn is converging to infinity and is infinite
+    if (mod > 2) return true; //zn is converging to infinity and is not part of the set as it is getting exponentially larger
     else return false; //zn is currently finite
 }
 int createSet(const double r, const double i) {
@@ -27,43 +27,20 @@ int createSet(const double r, const double i) {
         zr = (zr * zr) + (zi * zi * -1) + r; //when squaring complex numbers the real component is the real numbers squared and the imaginary component squared. Imaginary component is multiplied by -1 as i squared = -1
         if (zr == 0) //if there is no zr component the way zi gets computed is different (cannot expand the brackets) 
         {
-            zr = (zi * zi * -1);
-            zi = i;
+            zr = (zi * zi * -1); //i^2 is equal to -1 on the real plane (zn^2)
+            zi = i; //+c
         }
         else zi = (2 * tempr * zi) + i; //mandelbrot set is defined as zn^2 + c = zn+1 (i.e the next complex number is equal to the prior complex number + a constant complex number)
         n++; //when squaring imaginary component it is 2 multiples of zr * zi
     }
-    if (n == 25) {
+    if (n == 50) {
         //number of iterations reached, meaning that zn is finite as it is not getting larger and larger
-        return 25;
+        return 50;
     }
     else {
         return n;
     }
 }
-
-void threadedManderbrot(SDL_Renderer* r, double start, double end) {
-    /*auto s = chrono::system_clock::now();
-    time_t threadTime = chrono::system_clock::to_time_t(s);
-    cout << threadTime << endl;*/
-    for (double i = start; i < end; i += 0.001) {
-        for (double j = start; j < end; j += 0.001) {
-            double point_x = lerp(-2, 2, i);
-            double point_y = lerp(-2, 2, j);
-            int iterations = createSet(point_x, point_y);
-            if (iterations == 25)
-            {
-                SDL_SetRenderDrawColor(r, 0, 0, 0, 255); //black used to show c is within set
-                SDL_RenderDrawPointF(r, i * 1000, j * 1000);
-            }
-            else {
-                SDL_SetRenderDrawColor(r, 10 * iterations % 255, 10 * iterations % 255, 10 * iterations % 255, 255);
-                SDL_RenderDrawPointF(r, i * 1000, j * 1000);
-            }
-        }
-    }
-}
-
 int main()
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -73,24 +50,9 @@ int main()
     int height = 2000;
     SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
     SDL_RenderSetScale(renderer, 2, 2);
-    vector<thread> threads;
-    double partition = 0.01; //thread will do every 100 coordinates
-    double traversal = 0;
-    
-    //while (traversal < 1) {
-    //    int threadStart = traversal;
-    //    traversal += partition;
-    //    int threadEnd = traversal;
-    //    threads.push_back(thread(&threadedManderbrot, renderer, threadStart, threadEnd)); //establish a new thread
-    //}
-    //for (auto& thread : threads) {
-    //    thread.join();
-    //}
-    
-    
-    for (double i = 0; i < 1; i += 0.001) {
+    for (double i = 0; i < 1; i += 0.001) { //0-1 is the axis of the SDL window increased by 0.001 along each axis
         for (double j = 0; j < 1; j += 0.001) {
-            double point_x = lerp(-2, 2, i);
+            double point_x = lerp(-2, 2, i); //lerp converts from SDL axis to complex number axis (between -2 through to 2)
             double point_y = lerp(-2, 2, j);
             int iterations = createSet(point_x, point_y);
             if (iterations == 50) 
@@ -99,8 +61,8 @@ int main()
                 SDL_RenderDrawPointF(renderer, i * 1000, j * 1000);
             }
             else {
-                SDL_SetRenderDrawColor(renderer, 10 * iterations % 255, 10 * iterations % 255, 10 * iterations  % 255, 255);
-                SDL_RenderDrawPointF(renderer, i * 1000, j * 1000);
+                SDL_SetRenderDrawColor(renderer, 10 * iterations % 255, 10 * iterations % 255, 10 * iterations  % 255, 255); //lighter colours means complex number is less likely to get infinitely large
+                SDL_RenderDrawPointF(renderer, i * 1000, j * 1000); //converts back from complex to SDL coordinates
             }
         }
     }
